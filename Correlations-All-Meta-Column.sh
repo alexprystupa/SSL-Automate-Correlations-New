f7cf27c4facf6cede1312eba36a50823bd8fc801
Aleksandr Prystupa <alex.prystupa@gmail.com>
2024-07-02 14:07:16 -0400

    Update Correlations-All-Meta-Column.sh

--- Correlations-All-Meta-Column.sh
+++ Correlations-All-Meta-Column.sh
@@ -5,8 +5,6 @@ files=(./utilities/fold_creation/julia-as-training-all-folds-V3/lung_lymph_node/
 for filename in "${files[@]}"
 do
 	v=$(echo $filename | cut -d "f" -f 3 | cut -d "/" -f 3)
-	#echo ${v::-1}
-	#echo $v
 	sbatch Correlations.sh ${v::-1} 1
 done
 
